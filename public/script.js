6ecbe069c169f83e4e4dbabbb5941b0aef116967
Alexander Sagen <alexrsagen@gmail.com>
2015-06-21 04:07:12 +0200

    Added a pretty login page

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-angular.module("NodeCP", []);
+var app = angular.module("NodeCP", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when("/login", {
+			templateUrl: "pages/login.html",
+			controller: "loginController"
+		})
+});
+
+app.controller("loginController", function ($scope) {
+	$scope.login = function () {
+
+	};
+	
+	$(".ui.checkbox").checkbox();
+});
